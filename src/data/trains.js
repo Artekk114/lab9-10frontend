10ea379a306ae0e7761179cacfef6991aea39b04
Oleksandr Dubyk <uradubik66@gmail.com>
2026-05-13 12:44:58 +0300

    Початок роботи, добавлено тестові дані train.js

---
+++ src/data/trains.js
@@ -0,0 +1,38 @@
+export const trainsData = [
+  {
+    id: '1',
+    number: '705К',
+    route: { from: 'Київ', to: 'Львів' },
+    departure: '2023-11-20T06:00:00',
+    arrival: '2023-11-20T11:09:00',
+    duration: '5 год 9 хв',
+    type: 'Інтерсіті+',
+  },
+  {
+    id: '2',
+    number: '038Ш',
+    route: { from: 'Одеса', to: 'Київ' },
+    departure: '2023-11-20T21:10:00',
+    arrival: '2023-11-21T06:15:00',
+    duration: '9 год 5 хв',
+    type: 'Нічний експрес',
+  },
+  {
+    id: '3',
+    number: '164О',
+    route: { from: 'Харків', to: 'Дніпро' },
+    departure: '2023-11-20T14:30:00',
+    arrival: '2023-11-20T18:45:00',
+    duration: '4 год 15 хв',
+    type: 'Швидкісний',
+  },
+  {
+    id: '4',
+    number: '043Л',
+    route: { from: 'Івано-Франківськ', to: 'Київ' },
+    departure: '2023-11-20T20:00:00',
+    arrival: '2023-11-21T08:50:00',
+    duration: '12 год 50 хв',
+    type: 'Швидкий',
+  }
+];
